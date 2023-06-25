c7c3ea5c7c0b9ed0cf0a0fa1db6a1206d4d2df13
Chitlange Sahas <chitlangesahas@gmail.com>
2023-06-25 16:19:11 -0700

    feat: websocket sends data and we can decode it into Swift object

---
+++ FindMyCat/Models/WebsocketPayloadWrapper.swift
@@ -0,0 +1,29 @@
+//
+//  WebsocketPayloadWrapper.swift
+//  FindMyCat
+//
+//  Created by Sahas Chitlange on 6/25/23.
+//
+
+import Foundation
+
+enum PayloadType: String, Decodable {
+    case device
+    case position
+}
+
+struct PayloadWrapper: Decodable {
+    let devices: [Device]?
+    let positions: [Position]?
+    
+    private enum CodingKeys: String, CodingKey {
+        case devices
+        case positions
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        devices = try container.decodeIfPresent([Device].self, forKey: .devices)
+        positions = try container.decodeIfPresent([Position].self, forKey: .positions)
+    }
+}
---
+++ FindMyCat/network/WebSocketManager.swift
@@ -0,0 +1,65 @@
+//
+//  WebSocketManager.swift
+//  FindMyCat
+//
+//  Created by Sahas Chitlange on 6/25/23.
+//
+
+import Starscream
+import Combine
+import Foundation
+
+class WebSocketManager: WebSocketDelegate {
+    
+    let urlString = "ws://ec2-18-191-185-127.us-east-2.compute.amazonaws.com:8082/api/socket"
+    
+    private var socket: WebSocket?
+    private var cancellables = Set<AnyCancellable>()
+
+    private var isConnected: Bool = false;
+    private let subject = PassthroughSubject<String, Never>()
+
+    var dataPublisher: AnyPublisher<String, Never> {
+        return subject.eraseToAnyPublisher()
+    }
+
+    func connect() {
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.timeoutInterval = 5
+        socket = WebSocket(request: request)
+        socket?.delegate = self
+        socket?.connect()
+    }
+
+    func disconnect() {
+        socket?.disconnect()
+        socket = nil
+    }
+
+    func didReceive(event: Starscream.WebSocketEvent, client: Starscream.WebSocketClient) {
+      switch event {
+      case .connected(let headers):
+        print("connected \(headers)")
+      case .disconnected(let reason, let closeCode):
+        print("disconnected \(reason) \(closeCode)")
+      case .text(let text):
+//        print("received text: \(text)")
+        subject.send(text)
+      case .binary(let data):
+        print("received data: \(data)")
+      case .pong(let pongData):
+        print("received pong: \(pongData)")
+      case .ping(let pingData):
+        print("received ping: \(pingData)")
+      case .error(let error):
+        print("error \(error)")
+      case .viabilityChanged:
+        print("viabilityChanged")
+      case .reconnectSuggested:
+        print("reconnectSuggested")
+      case .cancelled:
+        print("cancelled")
+      }
+    }
+}
