501ec6bc093fe7e1b0b5cdef0343397d12bc88d6
Chitlange Sahas <chitlangesahas@gmail.com>
2023-07-06 23:16:38 -0700

    chore: file cleanup; made new files

---
+++ FindMyCat/Models/UWBMessages.swift
@@ -0,0 +1,29 @@
+//
+//  UWBMessages.swift
+//  FindMyCat
+//
+//  Created by Sahas Chitlange on 7/6/23.
+//
+
+import Foundation
+
+// An example messaging protocol for communications between the app and the
+// accessory. In your app, modify or extend this enumeration to your app's
+// user experience and conform the accessory accordingly.
+enum MessageId: UInt8 {
+    // Messages from the accessory.
+    case accessoryConfigurationData = 0x1
+    case accessoryUwbDidStart = 0x2
+    case accessoryUwbDidStop = 0x3
+
+    // Messages to the accessory.
+    case initialize = 0xA
+    case configureAndStart = 0xB
+    case stop = 0xC
+
+    // User defined/notification messages
+    case getReserved = 0x20
+    case setReserved = 0x21
+
+    case iOSNotify = 0x2F
+}
